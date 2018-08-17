792a9daa6b603edd816a8b6c21b4e4f2dac3df20
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-17 16:55:50 -0400

    added initial theme values

---
+++ lib/theme.ts
@@ -0,0 +1,33 @@
+export const theme = {
+  text: {
+    size: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24
+    },
+    weight: {
+      light: "200",
+      normal: "400",
+      bold: "700"
+    }
+  },
+  color: {
+    red: "#ff4545",
+    blue: "blue",
+    purple: "purple"
+  },
+  space: {
+    xs: 8,
+    sm: 16,
+    md: 24,
+    lg: 36,
+    xl: 40
+  },
+  button: {
+    disabled: {
+      opacity: 0.5
+    }
+  }
+};
